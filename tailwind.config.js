d422d23c1112a87f1d533fb13f4c476470a7cb2e
SaraChavezCh <sarachavez856@gmail.com>
2023-02-20 19:29:14 -0600

    project configurations

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT( {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "my-custom-color": "#fffff",
+      },
+      screens: {
+        xs: "375px",
+        sm: "600px",
+        md: "900px",
+        lg: "1200px",
+        xl: "1536px",
+      },
+    },
+  },
+  plugins: [],
+});
